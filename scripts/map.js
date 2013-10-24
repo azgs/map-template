9cc97951d9422de17b475ae29e314e912ad6a780
Ryan Clark <ryan.clark.j@gmail.com>
2013-10-24 16:43:03 -0700

    add legend toggle button

--- scripts/map.js
+++ scripts/map.js
@@ -1,8 +1,15 @@
 var app = {
   map: L.map('map', {center: [34, -111], zoom: 6}),
-  baseLayer: L.tileLayer('http://{s}.tiles.mapbox.com/v3/some-basemap-id/{z}/{x}/{y}.png', {
+  //baseLayer: L.tileLayer('http://{s}.tiles.mapbox.com/v3/some-basemap-id/{z}/{x}/{y}.png', {
+  baseLayer: L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '<a href="https://www.mapbox.com/about/maps/">Terms & Feedback</a>'
   })
 };
 
 app.baseLayer.addTo(app.map);
+
+// Add click effect to the legend toggler
+d3.select('#toggle-info').on('click', function () {
+  var enabled = d3.select('#info').classed('hidden');
+  d3.select('#info').classed('hidden', !enabled);
+});
